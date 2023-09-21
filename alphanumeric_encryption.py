87fa481c13058100b378e3f56e3ba21c6266d8a9
Loren Hicks <lorenhicks@gmail.com>
2023-09-20 21:45:44 -0400

    Add files via upload

---
+++ alphanumeric_encryption.py
@@ -0,0 +1,19 @@
+def encryption_key(word)
+    word = input()
+    password = ''
+    
+    for alphanumeric in word:
+        if alphanumeric == 'i':
+            password = password + '!'
+        elif alphanumeric == 'a':
+            password = password + '@'
+        elif alphanumeric == 'm':
+            password = password + 'M'
+        elif alphanumeric == 'B':
+            password = password + '8'
+        elif alphanumeric == 'o':
+            password = password + '.'
+        else:
+            password = password + alnum
+    
+    return(password)
